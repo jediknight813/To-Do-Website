005423fbd4be412213b920505d2f74159dd67d20
-THE-LAST-SAMURAI- <connor813@outlook.com>
2021-08-12 11:33:20 -0500

    updated the adding task system

--- Javascript.js
+++ Javascript.js
@@ -1,57 +1,56 @@
-events = [{"project" : "War", "date" : "1/1/1", "task" : "eat food"},{"project" : "War", "date" : "1/1/1", "task" : "eat food"},{"project" : "War", "date" : "1/1/1", "task" : "eat food"}];
 
 
-
-function to_add_task() {
-    document.getElementById("page_title").innerHTML = current_project
-    task = document.getElementById('task').value
-    date = document.getElementById('date').value
-    events += {"project" : current_project, "date" : date, "task" : task}
-}
+const events = [{"project" : "War", "date" : "1/1/1", "task" : "eat food"}];
 
 
 function project_page(project_name) {
     document.getElementById("page_title").innerHTML = project_name
-    add_task(events[0])
 }
 
 
 function add_task() {
-    events[0] = task
+    console.log(events)
 
-    let = project = task[0]
-    let = date = task[1]
-    let = task = task[2]
+    task = document.getElementById('task').value
+    date = document.getElementById('date').value
+    current_project = document.getElementById("page_title").innerHTML
+    events.push({"project" : current_project, "date" : date, "task" : task})
 
 
     var div = document.createElement("div");
-    let btn = document.createElement("button");
+    var task_text = document.createElement("div");
     var check_box = document.createElement('input')
+
+    var date_box = document.createElement('div')
+
+    date_box.type = "date"
+    date_box.className = "date_box"
+
+    task_text.className = 'task_text'
     check_box.type = 'checkbox'
     check_box.className = 'checkbox'
     check_box.id = "Finished"
+    div.className = "task_background"
 
 
-    btn.onclick = function(div, btn){
+    check_box.onclick = function(div){
         this.parentNode.remove(this.parentNode)
         this.remove(this)
-        reset_input_for_new_book()
+        reset_input_for_new_task()
     }
 
-    btn.innerHTML = "Task Finished"
-    btn.className = 'finished_button'
-    div.className = "task_background"
-    div.innerText =  task +  project + date + 
+    task_text.innerText = task  
+    date_box.innerText = date  
     div.appendChild(check_box)
-    div.appendChild(btn)
+    div.appendChild(task_text)
+    div.appendChild(date_box)
     document.getElementById("screen").appendChild(div)
-    reset_input_for_new_book()
+    reset_input_for_new_task()
 }
 
 
-function reset_input_for_new_book() {
+function reset_input_for_new_task() {
     document.getElementById('task').value = ''
     document.getElementById('date').value = ''
     document.getElementById('Finished').checked = false;
-
 }
